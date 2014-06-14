4f19a1a5b325b1562a65e4818b1c4b3ed4f992a7
Veikko Karsikko <veikko88@gmail.com>
2014-06-14 10:41:09 +0300

    hyphenator to class

--- src/jquery.finnishhyphenator.js
+++ src/jquery.finnishhyphenator.js
@@ -13,53 +13,56 @@
  */
 
 ;(function($, window, document, undefined) {
-	var vowels = "aeiouyåäö";
-	var consonants = "bcdfghjklmnpqrstvwxyz";
-	var hyphMark = "&shy;"; 
-	// construct regExps only once
-	var rule1RegExp = new RegExp("[" + consonants + "][" + vowels + "]", "gi");
-	var endReg = new RegExp("[" + consonants + vowels + "]{2}$", "i");
-
 	var pluginName = "finnishHyphenator";
+	var finnishHyphenator = null;
 
-	var addHyphen = function(text, idx) {
+	function FinnishHyphenator(){
+		this.vowels = "aeiouyåäö";
+		this.consonants = "bcdfghjklmnpqrstvwxyz";
+		this.hyphMark = "&shy;"; 
+		// construct regExps only once
+		this.rule1RegExp = new RegExp("[" + consonants + "][" + vowels + "]", "gi");
+		this.endReg = new RegExp("[" + consonants + vowels + "]{2}$", "i");
+		this.$wrapperElement = $("<div></div>");
+
+	}
+	FinnishHyphenator.prototype.addHyphen = function(text, idx) {
 		var firstPart = text.slice(0, idx);
-		if (endReg.exec(firstPart)) {
-			return (text.slice(0, idx) + hyphMark + text.slice(idx));
+		if (this.endReg.exec(firstPart)) {
+			return (text.slice(0, idx) + this.hyphMark + text.slice(idx));
 		} else {
 			return false;
 		}
-	};
- 	
-	var $wrapperElement = $("<div></div>");
-	var hyphenateElement = function($el) {
+	}
+	FinnishHyphenator.prototype.hyphenateElement = function($el) {
+		var classScope = this;
 		$el.contents().each(function() {
 			var $node = $(this);
 			if (this.nodeType === 3) {
 				// is text element and will be hyphenated
-				var hyphenated = hyphenateText(this.textContent);
+				var hyphenated = classScope.hyphenateText(this.textContent);
 				$wrapperElement.html(hyphenated);
 				$node.before($wrapperElement.html());
 				$node.remove();
 			} else {
-				hyphenateElement($node);
+				classScope.hyphenateElement($node);
 			}
 		});
-	};
+	}
 
-	var hyphenateText = function(text) {
+	FinnishHyphenator.prototype.hyphenateText = function(text) {
 		var hyphenated = text;
 		var hyphPositions = [];
 
 		// rule [1]
-		while ((match = rule1RegExp.exec(text)) != null) {
+		while ((match = this.rule1RegExp.exec(text)) != null) {
 			hyphPositions.push(match.index);
 		}
 
 		var count = 0;
 		for (var i = 0; i < hyphPositions.length; i++) {
-			var position = hyphPositions[i] + count * hyphMark.length;
-			var triedHyphenation = addHyphen(hyphenated, position);
+			var position = hyphPositions[i] + count * this.hyphMark.length;
+			var triedHyphenation = this.addHyphen(hyphenated, position);
 			if (triedHyphenation !== false) {
 				count++;
 				hyphenated = triedHyphenation;
@@ -69,6 +72,9 @@
 	};
 
 	function Plugin(element) {
+		if(!finnishHyphenator){
+			finnishHyphenator = new FinnishHyphenator();
+		}
 		this.element = element;
 		this._name = pluginName;
 		this.init();
@@ -76,7 +82,7 @@
 
 	$.extend(Plugin.prototype, {
 		init: function() {
-			hyphenateElement($(this.element));
+			finnishHyphenator.hyphenateElement($(this.element));
 		}
 	});
 	
